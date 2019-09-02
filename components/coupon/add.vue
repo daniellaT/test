ca3ee575d4de03e86360d21df4f9cd96bf728fce
lilgaetan <lilgaetan88@gmail.com>
2019-09-02 17:17:08 -0400

    new commit

--- components/coupon/add.vue
+++ components/coupon/add.vue
@@ -43,7 +43,7 @@ export default {
             valid : true,
             form : {
                 code_number:null,
-                code: 3
+                code: null
             }
         }
     },
--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://neweratutorapi.herokuapp.com/api/'
+    baseURL: 'http://127.0.0.1:8000/api/'
   },
 
   auth: {
