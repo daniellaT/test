b822bd41e2becaaf2ad5ce9901cd6ec7cf742dfc
lilgaetan <lilgaetan88@gmail.com>
2019-10-20 22:20:30 -0400

    =new commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://tutorfrontend.herokuapp.com/api/'
+    baseURL: 'https://neweraadminapi.herokuapp.com/api/'
   },
 
   auth: {
